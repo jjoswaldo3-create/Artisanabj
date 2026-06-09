282031b74069e46cc2ffa234f3d1c3e64c21bf25
jjoswaldo3-create <jjoswaldo3@gmail.com>
2026-06-09 08:28:57 +0000

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+})
+
+module.exports = withPWA({
+  images: {
+    domains: ['res.cloudinary.com', 'i.pravatar.cc']
+  }
+})
